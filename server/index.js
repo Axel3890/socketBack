bf9100dc093780ccadb944d75606812b2d92983a
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-04-27 16:12:22 -0300

    preuba 2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 3001;
 const io = require("socket.io")(port, {
     cors: {
-    origin: "https://socket-front-eight.vercel.app/",
+    origin: "socket-front-r0almdr0d-axel-gomezs-projects.vercel.app",
     methods: ["GET", "POST"],
 }})
 
@@ -10,6 +10,7 @@ io.on("connection", (socket) => {
 
 
     socket.on('message', (body) => {
+        console.log(body)
         socket.broadcast.emit('message', {
             body,
             from: socket.id.slice(6)
