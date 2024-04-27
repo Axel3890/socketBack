dd1a06f47cd97e4a3851e895b79bcbde7761a60f
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-04-27 15:42:56 -0300

    modificada ruta

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 3001;
 const io = require("socket.io")(port, {
     cors: {
-    origin: "http://localhost:3000",
+    origin: "https://socket-front-eight.vercel.app/",
     methods: ["GET", "POST"],
 }})
 
